6cc720efc61511f5563202d8eaabe7dd6b9672a8
Daftey <david@movadex.com>
2022-08-12 18:40:25 +0300

    change domen, fix sponsored article on light mode

--- pages/api/getCategories.js
+++ pages/api/getCategories.js
@@ -1,7 +1,7 @@
 import fetch from "isomorphic-unfetch";
 
 export default async function getCategories() {
-  const resp = await fetch("https://flow-blog.netlify.app/parsed/categories.json")
+  const resp = await fetch("https://thecymes.netlify.app/parsed/categories.json")
 
   if (resp.ok) {
     return await resp.json()
--- pages/api/getPosts.js
+++ pages/api/getPosts.js
@@ -1,7 +1,7 @@
 import fetch from "isomorphic-unfetch";
 
 export default async function getPosts() {
-    const resp = await fetch("https://flow-blog.netlify.app/parsed/posts.json")
+    const resp = await fetch("https://thecymes.netlify.app/parsed/posts.json")
 
   if (resp.ok) {
     return await resp.json()
