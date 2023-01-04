45066ccb1ef04d2c0698a5a0d21a901162066f72
Daftey <david@movadex.com>
2023-01-04 17:13:36 +0200

    gTag added

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,15 @@ class MyDocument extends Document {
           <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
           {/*<link href="/fonts/SpartanMB/style.css" rel="stylesheet"/>*/}
           <link rel="icon" href="/favicon.ico"/>
+          <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-SB0XJLQX6J"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+
+            gtag('config', 'G-SB0XJLQX6J');
+          </script>
         </Head>
         <body>
         <Main/>
